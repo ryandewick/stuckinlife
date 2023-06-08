cf03dace00f04a58d05d18e929ad437fab54ce21
Ryan Dewick <ryan.dewick@outlook.com>
2023-06-08 17:29:17 +0100

    coming soon

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <coming-soon></coming-soon>
+  </div>
+</template>
+
+<script lang="ts">
+import ComingSoon from "./components/ComingSoon.vue";
+
+export default {
+  components: {
+    ComingSoon,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 10px;
+}
+
+body {
+  // height: 100dvh;
+  // width: 100dvw;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  // background-color: rgb(36, 36, 36);
+  background-color: #3d97c5;
+}
+</style>
---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="coming-soon">
+    <img src="../assets/stuckinlife.webp" />
+    <p class="coming-soon__header">COMING SOON!</p>
+    <span class="coming-soon__motto"
+      >A helping hand and a guide for those lost in life</span
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped>
+img {
+  filter: brightness(10) saturate(100%);
+  width: 200px;
+}
+
+.coming-soon {
+  &__header {
+    color: white;
+    font-size: 4rem;
+  }
+
+  &__motto {
+    color: white;
+    font-size: 2rem;
+  }
+}
+</style>
