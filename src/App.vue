4100a5ced3cdb5742c50a04ab500c4caf3be4445
Ryan Dewick <ryan.dewick@hermes-europe.co.uk>
2023-08-16 12:45:39 +0100

    container for nav added

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,15 @@
 <template>
   <div>
-    <header>
-      <stuck-header />
-      <stuck-nav
-        class="stuck-nav"
-        :nav-items="navItems"
-        :button-text="buttonText"
-      />
-    </header>
+    <nav-bar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { stuckHeader, stuckNav } from "stuckinlife-components";
+import NavBar from "./components/NavBar.vue";
 export default {
   components: {
-    stuckHeader,
-    stuckNav,
-  },
-
-  data() {
-    return {
-      navItems: [
-        {
-          name: "Courses",
-          link: "/courses",
-        },
-        {
-          name: "About Us",
-          link: "/about-us",
-        },
-        {
-          name: "FAQ",
-          link: "/faq",
-        },
-        {
-          name: "Videos",
-          link: "/videos",
-        },
-      ],
-      buttonText: {
-        name: "Hello there, Ryan Dewick!",
-        link: "/profile",
-      },
-    };
+    NavBar,
   },
 };
 </script>
-
-<style scoped lang="scss">
-@import "@/assets/mixins/breakpoints";
-
-.stuck-nav {
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  z-index: 50;
-
-  @include desktop {
-    position: relative;
-  }
-}
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <header>
+    <stuck-header />
+    <stuck-nav
+      class="stuck-nav"
+      :nav-items="navItems"
+      :button-text="buttonText"
+    />
+  </header>
+</template>
+
+<script>
+import { stuckHeader, stuckNav } from "stuckinlife-components";
+export default {
+  components: {
+    stuckHeader,
+    stuckNav,
+  },
+
+  data() {
+    return {
+      navItems: [
+        {
+          name: "Courses",
+          link: "/courses",
+        },
+        {
+          name: "About Us",
+          link: "/about-us",
+        },
+        {
+          name: "FAQ",
+          link: "/faq",
+        },
+        {
+          name: "Videos",
+          link: "/videos",
+        },
+        {
+          name: "Test",
+          link: "/test",
+        },
+      ],
+      buttonText: {
+        name: "Hello there, Ryan Dewick!",
+        link: "/profile",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/mixins/breakpoints";
+
+.stuck-nav {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 50;
+
+  @include desktop {
+    position: relative;
+  }
+}
+</style>
